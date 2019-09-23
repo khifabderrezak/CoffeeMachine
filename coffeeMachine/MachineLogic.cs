513727156cb9a84737712167aa80bf9a1e09912e
Abderrezak MSI Melissa <abderrezak.khif@gmail.com>
2019-09-23 21:21:09 +0200

    commit third iteration

---
+++ coffeeMachine/MachineLogic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMachine
+{
+    public class MachineLogic
+    {
+        private readonly IDrinkMakerProtocol drinkMakerProtocol;
+        private readonly Cashier cashier;
+
+        public MachineLogic(IDrinkMakerProtocol drinkMakerProtocol)
+        {
+            this.drinkMakerProtocol = drinkMakerProtocol;
+            cashier = new Cashier();
+        }
+
+        public string SendOrder(IDrinkOrder drinkOrder)
+        {
+            var drinkprice = drinkOrder.GetDrinkPrice();
+            if (!IsPayed(drinkprice))
+            {
+                return MessageInsufficientAmount(drinkprice);
+            }
+            this.drinkMakerProtocol.SetCode(drinkOrder.GetDrinkCode());
+            this.drinkMakerProtocol.SetSugarQty(drinkOrder.GetDrinkSugarQty());
+
+            return this.drinkMakerProtocol.MakeOrder();
+        }
+
+        public string SendMessage(string message)
+        {
+            return this.drinkMakerProtocol.BuildMessage(message);
+        }
+
+        public string MessageInsufficientAmount(double drinkPrice)
+        {
+
+            double missingAmount = this.cashier.GetDifferenceWith(drinkPrice);
+            return this.SendMessage(string.Format("{0:0.00}", missingAmount));
+        }
+
+        public bool IsPayed(double drinkPrice)
+        {
+            return this.cashier.GetDifferenceWith(drinkPrice) <= 0;
+        }
+
+        public void InsertMoney(double money)
+        {
+            this.cashier.AddMoney(money);
+        }
+    }
+}
---
+++ coffeeMachine/Models/Tea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMachine
+{
+    public class Tea : HotDrink
+    {
+        public Tea() : base("T", .40)
+        {
+
+        }
+    }
+}
